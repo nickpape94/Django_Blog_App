f1cc7f6b612cd0d0c87c7eb584aca1a700f6eb8c
nickpape94 <nick-_p@hotmail.com>
2019-10-09 15:29:22 +0100

    Viewing profile now requires login authorisation

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegistrationFrom
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -15,4 +16,9 @@ def register(request):
     else:
         form = UserRegistrationFrom()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
